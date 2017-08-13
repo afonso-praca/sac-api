ba51b23b4864559fb330deea0cd5345c9b1ff156
afonso <afonsoinfo@gmail.com>
2017-08-12 23:16:54 -0300

    creates ticket controller, and implements save, get all, and delete

---
+++ controllers/ticket.js
@@ -0,0 +1,62 @@
+var mongoose = require("mongoose");
+require("../models/ticket");
+var Ticket = mongoose.model("Ticket");
+
+var createTicket = function (req, res) {
+  var ticket = new Ticket(req.body);
+  console.log(ticket);
+  ticket.save(function (err) {
+    if (!err) {
+      console.log("created");
+      return res.status(201).jsonp(ticket);
+    } else {
+      console.log(err);
+      return res.status(500);
+    }
+  });
+};
+
+var getAllTickets = function (req, res) {
+  Ticket.find(function (err, tickets) {
+    if (err) return console.error(err);
+    res.send(tickets);
+  })
+};
+
+var deleteTicket = function (req, res) {
+  return Ticket.findById(req.params.id, function (err, ticket) {
+    if (err !== null){
+      return res.status(404).send("no ticket found");
+    } else {
+      if (ticket !== null) {
+        return ticket.remove(function (err) {
+          if (!err) {
+            console.log("removed");
+            return res.send("removed");
+          } else {
+            console.log(err);
+            return res.status(500);
+          }
+        });
+      } else {
+        console.log(err);
+        return res.status(404).send("no ticket found");
+      }
+    }
+  });
+};
+
+module.exports = {
+  getAllTickets: function (req, res) {
+    getAllTickets(req, res);
+  },
+  getTicket: function (req, res) {
+
+  },
+  deleteTicket: function (req, res) {
+    deleteTicket(req, res);
+  },
+  createTicket: function (req, res) {
+    createTicket(req, res);
+  }
+};
