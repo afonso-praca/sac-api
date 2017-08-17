dde3c9978e5f7e9b06c3b61de4f52fcd04d4678c
afonso <afonsoinfo@gmail.com>
2017-08-16 23:33:24 -0300

    mark required fields

--- models/ticket.js
+++ models/ticket.js
@@ -2,11 +2,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TicketSchema = new Schema({
-  type: String,
-  state: String,
-  reason: String,
-  details: String,
-  createdAt: { type: Date, 'default': Date.now }
+  type: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  reason: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    'default': Date.now
+  }
 });
 
 module.exports = mongoose.model('Ticket', TicketSchema);
